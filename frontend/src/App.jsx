b489a6c536eb28d3f2479435994445b8f4ed3ff6
Pepe <jganavas@gmail.com>
2026-02-19 09:17:41 +0100

    Arranco proyecto React y preparo Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center flex-col">
+      <h1 className="text-3xl font-bold text-gray-800">
+        ¡Proyecto limpio y listo! 🚀
+      </h1>
+      <h2 className="text-2xl font-bold text-gray-800">a topeeeeee</h2>
+    </div>
+  )
+}
+
+export default App
