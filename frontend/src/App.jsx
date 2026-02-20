05578d53f4c8aadaa2fc7b0a0e72020f053c243b
Pepe <jganavas@gmail.com>
2026-02-20 09:27:40 +0100

    Actualizo cambios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
